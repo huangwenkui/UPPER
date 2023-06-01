c7592af0033d6bac44458b7a699aaf1b65efa762
huangwenkui <94673121+huangwenkui@users.noreply.github.com>
2023-06-01 19:16:03 +0800

    Add files via upload

---
+++ preprocess_snpdata/script/preprocess_reactome.py
@@ -0,0 +1,19 @@
+fp=open("03data_graph/reactome_db.graph.tsv","w")
+for line in open("03data_graph/reactome.graph.tsv"):
+    arr=line.strip().split("\t")
+    if len(arr)==3:
+        el1=arr[0].split("|")
+        el2=arr[2].split("|")
+        if len(el1)==1 and len(el2)==1:
+            if el1!=el2:
+                fp.write(line)
+        else:
+            for e1 in el1:
+                for e2 in el2:
+                    if e1 != e2:
+                        s="\t".join([e1,arr[1],e2])
+                        fp.write(s)
+                        fp.write("\n")
+    else:
+        print("ERROR")
+
